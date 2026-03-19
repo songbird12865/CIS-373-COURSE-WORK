250b1de13679b23c4d4558a2de3be20d01d729b7
songbird12865 <Mjhassan128@gmail.com>
2026-03-19 19:56:58 -0400

    Conversion to live #1

--- js/cis373.js
+++ js/cis373.js
@@ -5,36 +5,28 @@ function validateForm() {
         return false;
     }
     let x2 = document.forms["contactForm"]["email"].value;
-    if (x2 == "") {
-        alert("Email must be filled in");
-        return false;
-    }
-    let mailformat = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-    if (!document.forms["contactForm"]["email"].value.match(mailformat))
-    {
-        alert("You have entered an invalid email address");
-        return false;
-    }
     let x3 = document.forms["contactForm"]["phone"].value;
-    if (x3 == "") {
-        alert("Phone must be filled in");
+    if (x2 == "" && x3 == "") {
+        alert("Please provide either an email address or a phone number");
         return false;
+    }
+    if (x2 != ""){
+        let mailformat = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+        if (!x2.match(mailformat)) {
+            alert("You have entered an invalid email address");
+            return false;
+        }
+    }
+    if (x3 != "") {
+        let phoneformat = new RegExp('^\\(?\\d{3}\\)?[-.\\s]?\\d{3}[-.\\s]?\\d{4}$');
+        if (!x3.match(phoneformat)) {
+            alert("Please enter a valid phone number, e.g. (123) 456-7890");
+            return false;
+        }
     }
     let x4 = document.forms["contactForm"]["message"].value;
     if (x4 == "") {
         alert("Message must be filled in");
         return false;
     }
-}
-function getRadioValue(radio) {
-    alert("You selected: " + radio.value);
-}
-function getCheckboxClick(checkbox) {
-    alert("You selected: " + checkbox.value);
-}
-function getMenuSelection(menu) {
-    alert("You selected: " + menu.value)
-}
-function getTextboxEntry(textbox) {
-    alert("You entered: " + textbox.value)
 }
